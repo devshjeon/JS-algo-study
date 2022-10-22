3e796f50da8f496b64ebdb817d73d63d7936019b
Taekyeom Lee <tkyeom96@gmail.com>
2022-10-23 00:13:37 +0900

    섹션 8 재귀 문제

--- 4주/이태겸/8-10.js
+++ 4주/이태겸/8-10.js
@@ -3,6 +3,6 @@
 // power(2,4) // 16
 
 function power(a, b) {
-  if (b === 0) return 1;
-  return a * power(a, b - 1);
+  if (b === 0) return 1
+  return a * power(a, b - 1)
 }
---
+++ 4주/이태겸/8-11.js
@@ -0,0 +1,10 @@
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+function factorial(n) {
+  if (n === 0) return 1
+  else if (n === 1) return 1
+  return n * factorial(n - 1)
+}
