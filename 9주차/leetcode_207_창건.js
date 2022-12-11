0af13e28cfec02e13c32ccca535cbff69001d7db
Jackson <pcg0527@naver.com>
2022-12-11 12:40:11 +0900

    207

--- 9주차/leetcode_207_창건.js
+++ 9주차/leetcode_207_창건.js
@@ -1,12 +1,14 @@
-let node = head;
-let prev = null;
-let next;
+var reverseList = function (head) {
+  let node = head;
+  let prev = null;
+  let next;
 
-while (node) {
-  next = node.next;
-  node.next = prev;
-  prev = node;
-  node = next;
-}
+  while (node) {
+    next = node.next;
+    node.next = prev;
+    prev = node;
+    node = next;
+  }
 
-return prev;
+  return prev;
+};
